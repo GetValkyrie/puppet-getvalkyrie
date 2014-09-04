cc593c6e90baa48fd347e30b2cf748819b6612e0
Christopher Gervais <chris@ergonlogic.com>
2014-09-04 12:45:09 +0000

    Update data-only container scripts.

--- files/gitlab_backup.sh
+++ files/gitlab_backup.sh
@@ -5,13 +5,25 @@
 
 GITLAB_BACKUP_PATH=${GITLAB_BACKUP_PATH:=/container_data}
 
-# Remove last cached data.
-rm -rf $GITLAB_SOURCE_PATH/mysql
-if [ -e $GITLAB_SOURCE_PATH/git ]; then
-  rm -rf $GITLAB_SOURCE_PATH/git
+# Initialize backup path, if it doesn't already exist
+if [ ! -e $GITLAB_BACKUP_PATH/mysql ]; then
+  mkdir -p $GITLAB_BACKUP_PATH/mysql
 fi
+if [ ! -e $GITLAB_BACKUP_PATH/git ]; then
+  mkdir -p $GITLAB_BACKUP_PATH/git
+fi
+
+# Remove existing cached data.
+if [ -e $GITLAB_BACKUP_PATH/mysql ]; then
+  rm -rf $GITLAB_BACKUP_PATH/mysql
+fi
+if [ -e $GITLAB_BACKUP_PATH/git ]; then
+  rm -rf $GITLAB_BACKUP_PATH/git
+fi
+
 # Copy live data to our data cache.
-cp /var/lib/mysql $GITLAB_SOURCE_PATH/mysql -r
+cp /var/lib/mysql $GITLAB_BACKUP_PATH/mysql -r
 if [ -e /home/git/repositories ]; then
-  cp /home/git/repositories $GITLAB_SOURCE_PATH/git -r
+  cp /home/git/repositories $GITLAB_BACKUP_PATH/git -r
 fi
+
--- files/gitlab_restore.sh
+++ files/gitlab_restore.sh
@@ -6,14 +6,18 @@
 # default).
 
 GITLAB_BACKUP_PATH=${GITLAB_BACKUP_PATH:=/container_data}
+CURRENT_TIME=`date --iso-8601=seconds`
 
 # Move current data out of the way.
-mv /var/lib/mysql /var/lib/mysql.`date --iso-8601=seconds`
+mkdir /var/lib/mysql.$CURRENT_TIME
+mv /var/lib/mysql/* /var/lib/mysql.$CURRENT_TIME/
 if [ -e /home/git/repositories ]; then
-  mv /home/git/repositories /home/git/repositories.`date --iso-8601=seconds`
+  mkdir /home/git/repositories.$CURRENT_TIME
+  mv /home/git/repositories/* /home/git/repositories.$CURRENT_TIME/
 fi
 # Copy our cached data into place.
-cp $GITLAB_SOURCE_PATH/mysql /var/lib/mysql -r
-if [ -e $GITLAB_SOURCE_PATH/git ]; then
-  cp $GITLAB_SOURCE_PATH/git /home/git/repositories -r
+cp $GITLAB_BACKUP_PATH/mysql/* /var/lib/mysql/ -r
+if [ -e $GITLAB_BACKUP_PATH/git ]; then
+  cp $GITLAB_BACKUP_PATH/git/* /home/git/repositories/ -r
 fi
+
